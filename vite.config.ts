e9a9071a810ae7a903a4277a7d3247fbaee2a9c3
Aine Drelingyte <106792472+ADrelingyte@users.noreply.github.com>
2026-03-26 14:28:51 +0100

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig({
-  base: './',
+  base: '/Project-Kap-Nomelius/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
